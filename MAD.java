db26d2e5f818b413991e3884fbb9ce477e855fa6
Miled Chalal-Henri <miledch@gmail.com>
2020-05-24 19:00:43 -0400

    commit for iteration 1
    a main calculator menu with my function class

---
+++ MAD.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MAD 
+{
+    public static void MADfunction()
+    {
+        
+        ArrayList<Integer> userEntries = new ArrayList<Integer>();
+        System.out.println("Enter each entries belonging to the data set, simply press f when finished");
+        Scanner scanner = new Scanner(System.in);
+        String userEntry = new String();
+
+        while(!userEntry.equals("f"))
+        {
+            System.out.print("> ");
+            userEntry = scanner.nextLine();
+            if(EntryIsInteger(userEntry))
+            {
+                userEntries.add(Integer.parseInt(userEntry));
+            }
+            else
+            {
+                if(!userEntry.equals("f"))
+                    System.out.println("Please enter a numerical value, or f to finish");
+            }
+        }
+        // Step 1: Calculate the mean.
+        int sumOfEntries = 0;
+        for (int entry : userEntries) {
+            sumOfEntries += entry;
+        }
+        int mean = sumOfEntries / userEntries.size();
+
+        // Step 2: Calculate the distance between each data point and the mean.
+        for (int i = 0; i < userEntries.size(); i++) 
+        {
+            int distanceFromMean = Math.abs(userEntries.get(i) - mean);
+            userEntries.set(i, distanceFromMean);
+        }
+
+        // Step 3: Add the distances together.
+        int addedDistances = 0;
+        for (int entry : userEntries) {
+            addedDistances += entry;
+        }
+
+        // Step 4: Divide the sum by the number of data points.
+        float mad = (float)addedDistances / userEntries.size();
+
+        System.out.println("The Mean absolute deviation is: " + mad);
+
+        scanner.close();
+    }
+
+    private static boolean EntryIsInteger(String userEntry)
+    {
+        try 
+        {
+            Integer.parseInt(userEntry);
+            return true;
+        } 
+        catch (NumberFormatException nfe) 
+        {
+            return false;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,10 +1,64 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
 
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello World");
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        
+        DisplayMenu();
+        System.out.print("Please choose an operation > ");
+        int operation = scanner.nextInt();
+        while(operation < 1 || operation > 8)
+        {
+            System.out.println("Please choose a valid operation between 1 and 8");
+            operation = scanner.nextInt();
+        }
+
+        switch (operation) {
+            case 1:
+                // insert function here
+                break;
+            case 2:
+                // insert function here
+                break;
+            case 3:
+                // insert function here
+                break;
+            case 4:
+                // insert function here
+                break;
+            case 5:
+                MAD.MADfunction();
+                break;
+            case 6:
+                // insert function here
+                break;
+            case 7:
+                // insert function here
+                break;
+            case 8:
+                // insert function here
+                break;
+            default:
+                break;
+        }
+
+        scanner.close();
+    }
+
+    private static void DisplayMenu() {
+        System.out.print(System.lineSeparator());
+        System.out.println("Welcome to ETERNITY");
+        System.out.println("1. sin(x) XOR cos(x)");
+        System.out.println("2. 10\u02e3 XOR π\u02e3");
+        System.out.println("3. log\u2091(x) = ln(x) XOR log\u2081\u2080(x)");
+        System.out.println("4. e\u02e3 XOR a\u02e3");
+        System.out.println("5. MAD (Mean Absolute Deviation)");
+        System.out.println("6. σ (Standard Deviation)");
+        System.out.println("7. sinh(x) XOR cosh(x) XOR tanh(x)");
+        System.out.println("8. x\u02b8");
     }
 }
